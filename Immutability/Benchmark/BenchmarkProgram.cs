30e19fa482366fccac4fcb3a7d297321b65240cf
Matt Warren <matt.warren@live.co.uk>
2016-01-19 10:23:27 +0000

    Added initial Benchmark

---
+++ Immutability/Benchmark/BenchmarkProgram.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet;
+using System.Collections.Immutable;
+
+namespace ImmutabilityBenchmark
+{
+    public class BenchmarkProgram
+    {
+        static void Main(string[] args)
+        {
+            new BenchmarkRunner().Run<BenchmarkProgram>();
+        }
+
+        [Benchmark]
+        public Builder1.Person Builder1()
+        {
+            var jon = new Builder1.Person.Builder
+            {
+                Name = "Jon",
+                Address = new Builder1.Address.Builder { City = "Reading", Street = "..." }.Build(),
+                Phones = { }
+            }.Build();
+            return jon;
+        }
+
+        [Benchmark]
+        public Builder2.Person Builder2()
+        {
+            var jon = new Builder2.Person.Builder
+            {
+                Name = "Jon",
+                Address = new Builder2.Address.Builder { City = "Reading", Street = "..." }.Build(),
+                Phones = { }
+            }.Build();
+            return jon;
+        }
+
+        [Benchmark]
+        public Builder3.Person Builder3()
+        {
+            var jon = new Builder3.Person
+            {
+                Name = "Jon",
+                Address = new Builder3.Address { City = "Reading", Street = "..." },
+                Phones = { }
+            };
+            return jon;
+        }
+
+        [Benchmark("Withers")]
+        public Withers.Person WithersBenchmark()
+        {
+            var jon = new Withers.Person(
+                name: "Foo",
+                address: null, // Do this later
+                phones: new[] { new Withers.PhoneNumber("1235", Withers.PhoneNumberType.Home) }.ToImmutableList());
+            var later = jon.WithAddress(new Withers.Address("School Road", "Reading"));
+            return jon;
+        }
+    }
+}
